804a1355bfd19c410aa340805f1ca28a437fd74a
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-09-20 13:16:54 +0100

    add opengl fix

--- default.nix
+++ default.nix
@@ -2,6 +2,7 @@ with import <nixpkgs> {};
 
 let
   cairo-gl = cairo.override { glSupport = true; };
+  opengl-drivers = mesa_drivers;
 in stdenv.mkDerivation {
   name = "test";
   src = ./.;
@@ -9,5 +10,14 @@ in stdenv.mkDerivation {
   buildInputs = [
     clang pkg-config meson ninja
     glfw xorg.libX11 xorg.libXrandr cairo-gl
+    makeWrapper
   ];
+
+  postFixup = ''
+    for f in $out/bin/*; do
+      wrapProgram $f \
+        --prefix LD_LIBRARY_PATH : "${opengl-drivers}/lib" \
+        --set LIBGL_DRIVERS_PATH "${opengl-drivers}/lib/dri"
+    done
+  '';
 }
