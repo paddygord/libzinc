764a559cb5ecf5d78b8617d62af150accd28a582
Patrick Gordon <pgpatrickgordon@gmail.com>
2020-09-20 14:38:18 +0100

    rendering changes

--- test/cairo-test.cc
+++ test/cairo-test.cc
@@ -34,78 +34,84 @@ int main() {
         Window x11_window = glfwGetX11Window(window);
 
         cairo_device_t* cairo_device = cairo_glx_device_create(x11_display, glx_context);
-        surface = cairo_gl_surface_create_for_window(cairo_device, x11_window, 800, 800);
+        int window_width, window_height;
+        glfwGetWindowSize(window, &window_width, &window_height);
+        surface = cairo_gl_surface_create_for_window(cairo_device, x11_window, window_width, window_height);
         cairo_device_destroy(cairo_device);
         cr = cairo_create(surface);
     }
-    zinc::morton::AABB<2, 32> aabb = {256, 256 + 64 * 3 + 16 + 4};
+    zinc::morton::AABB<2, 32> aabb = {0, 1024 * 3};
+    cairo_matrix_t screen_mat, world_mat;
+    float scale = 16;
+    cairo_get_matrix(cr, &screen_mat);
+    cairo_scale(cr, scale, scale);
+    cairo_get_matrix(cr, &world_mat);
     while (!glfwWindowShouldClose(window)) {
+        cairo_set_source_rgb(cr, 1, 1, 1);
+        cairo_paint(cr);
         {
-            cairo_set_source_rgb(cr, 1, 1, 1);
-            cairo_paint(cr);
-            morton_code<2, 32> m {0};
-            float scale = 16;
-            cairo_save(cr);
-            cairo_translate(cr, scale, scale);
-            cairo_scale(cr, scale, scale);
-            for (uint32_t x = 0, y = 0; m.data < 1024; m.data++) {
-                cairo_move_to(cr, x, y);
+            cairo_set_matrix(cr, &world_mat);
+
+            cairo_translate(cr, 0.5, 0.5);
+            cairo_move_to(cr, 0, 0);
+            for (morton_code<2, 32> m = 0; m.data < 1024 * 4; m.data++) {
                 auto a = m.decode(m);
-                x = a[0];
-                y = a[1];
-                cairo_line_to(cr, x, y);
+                cairo_line_to(cr, a[0], a[1]);
             }
-            cairo_restore(cr);
+
+            cairo_set_matrix(cr, &screen_mat);
             cairo_set_line_width(cr, 1);
             cairo_set_source_rgb(cr, 0, 0, 0);
             cairo_stroke(cr);
-
-            cairo_save(cr);
-            cairo_translate(cr, scale, scale);
-            cairo_scale(cr, scale, scale);
-            cairo_translate(cr, -0.5, -0.5);
-
+        }
+        {
             double mouse_x, mouse_y;
             glfwGetCursorPos(window, &mouse_x, &mouse_y);
-            /*
-            mouse_x -= scale;
-            mouse_y -= scale;
-            */
-            mouse_x /= scale;
-            mouse_y /= scale;
-            if (GLFW_PRESS == glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT)) {
-                aabb.min = morton_code<2, 32>::encode({(uint32_t)mouse_x, (uint32_t)mouse_y});
-            } else if (GLFW_PRESS == glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT)) {
-                aabb.max = morton_code<2, 32>::encode({(uint32_t)mouse_x, (uint32_t)mouse_y});
-            } else if (GLFW_PRESS == glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE)) {
-                aabb.min = morton_code<2, 32>::encode({(uint32_t)mouse_x, (uint32_t)mouse_y});
+            uint32_t x_pos = (mouse_x - 0.5) / scale;
+            uint32_t y_pos = (mouse_y - 0.5) / scale;
+            auto m = morton_code<2, 32>::encode({x_pos, y_pos});
+            if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+                aabb.max = m;
+            } else if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS) {
+                aabb.min = m;
             }
-            {
-                uint32_t min_x, min_y, max_x, max_y;
-                std::array<uint32_t, 2> pack;
+        }
+        {
+            cairo_set_matrix(cr, &world_mat);
 
-                pack = morton_code<2, 32>::decode({aabb.min});
-                min_x = pack[0];
-                min_y = pack[1];
-                pack = morton_code<2, 32>::decode({aabb.max});
-                max_x = pack[0];
-                max_y = pack[1];
+            uint32_t min_x, min_y, max_x, max_y;
+            std::array<uint32_t, 2> pack;
+            pack = morton_code<2, 32>::decode({aabb.min});
+            min_x = pack[0];
+            min_y = pack[1];
+            pack = morton_code<2, 32>::decode({aabb.max});
+            max_x = pack[0];
+            max_y = pack[1];
+            cairo_rectangle(cr, min_x, min_y, max_x - min_x, max_y - min_y);
 
-                cairo_rectangle(cr, min_x, min_y, max_x - min_x, max_y - min_y);
-
-                auto i = aabb.get_first_interval();
-                pack = morton_code<2, 32>::decode({i.start});
-                min_x = pack[0];
-                min_y = pack[1];
-                pack = morton_code<2, 32>::decode({i.end});
-                max_x = pack[0];
-                max_y = pack[1];
-
-                cairo_rectangle(cr, min_x, min_y, 1, 1);
-                cairo_rectangle(cr, max_x, max_y, 1, 1);
-            }
-            cairo_restore(cr);
+            cairo_set_matrix(cr, &screen_mat);
             cairo_set_source_rgba(cr, 0.8, 0.9, 0.95, 0.5);
+            cairo_fill_preserve(cr);
+            cairo_set_source_rgb(cr, 0, 0, 0);
+            cairo_stroke(cr);
+        }
+        {
+            cairo_set_matrix(cr, &world_mat);
+
+            uint32_t min_x, min_y, max_x, max_y;
+            std::array<uint32_t, 2> pack;
+            auto i = aabb.get_first_interval();
+            pack = morton_code<2, 32>::decode({i.start});
+            min_x = pack[0];
+            min_y = pack[1];
+            cairo_rectangle(cr, min_x, min_y, 1, 1);
+            pack = morton_code<2, 32>::decode({i.end});
+            max_x = pack[0];
+            max_y = pack[1];
+            cairo_rectangle(cr, max_x, max_y, 1, 1);
+
+            cairo_set_matrix(cr, &screen_mat);
+            cairo_set_source_rgba(cr, 0.8, 0.0, 0.0, 0.5);
             cairo_fill_preserve(cr);
             cairo_set_source_rgb(cr, 0, 0, 0);
             cairo_stroke(cr);
@@ -116,31 +122,4 @@ int main() {
     }
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
-    {
-        zinc::morton::AABB<2, 32> aabb = {256, 256 + 64 * 3 + 16 + 4};
-        auto i = aabb.get_first_interval();
-        std::cout << i.start.data << ", " << i.end.data << std::endl;
-        std::cout << "expected:" << std::endl;
-        std::cout << 256 << ", " << 256 + 64 + 16 + 4 << std::endl;
-    }
-    {
-        zinc::morton::AABB<2, 32> aabb = {51, 193};
-        uint64_t litmax = 0, bigmin = 0;
-        std::tie(litmax, bigmin) = aabb.morton_get_next_address();
-        assert(litmax == 107);
-        assert(bigmin == 145);
-        aabb.max = 107;
-        std::tie(litmax, bigmin) = aabb.morton_get_next_address();
-        assert(litmax == 63);
-        assert(bigmin == 98);
-        aabb.min = 98;
-        std::tie(litmax, bigmin) = aabb.morton_get_next_address();
-        assert(litmax == 99);
-        assert(bigmin == 104);
-        aabb.min = 145;
-        aabb.max = 193;
-        std::tie(litmax, bigmin) = aabb.morton_get_next_address();
-        assert(litmax == 149);
-        assert(bigmin == 192);
-    }
 }
